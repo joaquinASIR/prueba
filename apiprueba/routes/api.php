ed263830428ead1c5ad1a39e1e94bda89e886f32
joaquinASIR <jok_zed@hotmail.com>
2021-02-06 19:33:27 +0100

    middl

--- apiprueba/routes/api.php
+++ apiprueba/routes/api.php
@@ -32,10 +32,10 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('registro', [AuthController::class, 'registro']);
 
 Route::get('user', [AuthController::class, 'getUser'])
-   ->middleware('auth:api');
+   
 
 Route::post('logout', [AuthController::class, 'logout'])
-    ->middleware('auth:api');
+    
     
 
 
